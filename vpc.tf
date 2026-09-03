2502ca4eb9def4413ce965c83c600d203d2d079b
Deepanshu Kumar <anshuk6469@gmail.com>
2026-09-03 16:24:34 +0530

    vpc.tf added

---
+++ vpc.tf
@@ -0,0 +1,7 @@
+resource "aws_vpc" "main" { #Resource Block -> Create any resource in terraform
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+  tags = {
+    Name = "deep-vpc"
+  }
+}
